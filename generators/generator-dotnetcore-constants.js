ecf1247321ad2f9b711323b88562db18d97eecb2
nicolas63 <nicolas_raymond@orange.fr>
2020-05-08 23:23:43 +0200

    Progress in blazor generation

--- generators/generator-dotnetcore-constants.js
+++ generators/generator-dotnetcore-constants.js
@@ -1,10 +1,12 @@
 const SERVER_SRC_DIR = 'src/';
+const CLIENT_SRC_DIR = 'src/';
 const SERVER_TEST_DIR = 'test/';
 const PROJECT_TEST_SUFFIX = '.Test';
 const DOCKER_DIR = 'docker/';
 
 const constants = {
     SERVER_SRC_DIR,
+    CLIENT_SRC_DIR,
     SERVER_TEST_DIR,
     PROJECT_TEST_SUFFIX,
     DOCKER_DIR,
